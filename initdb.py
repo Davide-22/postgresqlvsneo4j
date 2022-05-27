b4f6b6fa0694166ae6522e308749ed892cc82b28
Davide-22 <daviderenzetti59@gmail.com>
2022-05-27 17:50:00 +0200

    Initial commit

---
+++ initdb.py
@@ -0,0 +1,65 @@
+from importlib.resources import path
+import psycopg2
+from neo4j import GraphDatabase
+
+def initPostgres():
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            database="dmproj",
+            user="postgres",
+            password="postgres")
+        cur = conn.cursor()
+        q = """CREATE TABLE atenei (
+            _id int,
+            cod int,
+            nomeesteso VARCHAR(100),
+            nomeoperativo VARCHAR(100),
+            status VARCHAR(100),
+            tipologia VARCHAR(100),
+            statale_nonstatale VARCHAR(100),
+            indirizzo VARCHAR(100),
+            comune VARCHAR(100),
+            provincia VARCHAR(100),
+            regione VARCHAR(100),
+            zonageografica VARCHAR(100),
+            url VARCHAR(100),
+            dimensione VARCHAR(100),
+            PRIMARY KEY (_id)
+            );"""
+        cur.execute(q)
+        path = '/home/davide/Scrivania/postgresqlvsneo4j/csv/Università.csv'
+        q = """COPY atenei(_id, cod, nomeesteso, nomeoperativo,status,tipologia,statale_nonstatale,indirizzo,comune,provincia,regione,zonageografica,url,dimensione)
+            FROM stdin
+            DELIMITER ','
+            CSV HEADER;"""
+        with open(path, 'r') as f:
+            cur.copy_expert(sql=q, file=f)
+            conn.commit()
+        #rows = cur.fetchall()
+        #print(rows)    
+        cur.close()
+    except Exception as e:
+        print("Errore di connessione")
+        print(e)
+
+def execute_neo4j_query(q,session):
+    result = session.run(q)
+    return result
+
+
+def initNeo4j():
+    uri = "bolt://localhost:7687"
+    #CREATE CONSTRAINT ON (ateneo:ateneo) ASSERT ateno._id IS UNIQUE
+    driver = GraphDatabase.driver(uri, auth=("neo4j", "12345678"))
+    session = driver.session()
+    q = """LOAD CSV FROM 'file:///Università.csv' AS line FIELDTERMINATOR ','
+            CREATE (:ateneo {_id: toInteger(line[0]), cod: toInteger(line[1]), nomeesteso : line[2], nomeoperativo : line[3], status : line[4], tipologia : line[5], statale_nonstatale : line[6], indirizzo : line[7], comune : line[8], provincia : line[9], regione : line[10], zonageografica : line[11], url : line[12], dimensione : line[13]})"""
+    
+    r = execute_neo4j_query(q,session)
+    #for result in r:
+    #    print(result[0].labels)
+
+if __name__ == "__main__":
+    initPostgres()
+    #initNeo4j()
