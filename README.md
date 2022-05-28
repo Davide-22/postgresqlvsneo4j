c9d30a34ec9df1e19b599b10784938b3dc7a5edb
Davide-22 <daviderenzetti59@gmail.com>
2022-05-28 15:27:31 +0200

    Initdb

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# postgresqlvsneo4j
+A program to compare PostgreSQL and Neo4j performances.
--- initdb.py
+++ initdb.py
@@ -10,9 +10,12 @@ def initPostgres():
             user="postgres",
             password="postgres")
         cur = conn.cursor()
-        q = """CREATE TABLE atenei (
+        q = "DROP TABLE IF EXISTS atenei;"
+        cur.execute(q)
+        q = """
+            CREATE TABLE atenei (
             _id int,
-            cod int,
+            cod int UNIQUE,
             nomeesteso VARCHAR(100),
             nomeoperativo VARCHAR(100),
             status VARCHAR(100),
@@ -35,17 +38,35 @@ def initPostgres():
             CSV HEADER;"""
         with open(path, 'r') as f:
             cur.copy_expert(sql=q, file=f)
-            conn.commit()
         #rows = cur.fetchall()
-        #print(rows)    
+        #print(rows)   
+        q = "DROP TABLE IF EXISTS laureati;"
+        cur.execute(q) 
+        q = """CREATE TABLE laureati (
+            anno int,
+            codateneo int,
+            nomeateneo VARCHAR(100),
+            sesso VARCHAR(1),
+            numlaureati int,
+            CONSTRAINT fk_laureati
+                FOREIGN KEY(codateneo) 
+                    REFERENCES atenei(cod)
+                        ON DELETE CASCADE
+            );"""
+        cur.execute(q)
+        path = '/home/davide/Scrivania/postgresqlvsneo4j/csv/Laureati.csv'
+        q = """COPY laureati(anno,codateneo,nomeateneo,sesso,numlaureati)
+            FROM stdin
+            DELIMITER ';'
+            CSV HEADER;"""
+        with open(path, 'r') as f:
+            cur.copy_expert(sql=q, file=f)
+        conn.commit()
         cur.close()
     except Exception as e:
         print("Errore di connessione")
         print(e)
 
-def execute_neo4j_query(q,session):
-    result = session.run(q)
-    return result
 
 
 def initNeo4j():
@@ -53,13 +74,31 @@ def initNeo4j():
     #CREATE CONSTRAINT ON (ateneo:ateneo) ASSERT ateno._id IS UNIQUE
     driver = GraphDatabase.driver(uri, auth=("neo4j", "12345678"))
     session = driver.session()
+    q = "MATCH (n) DETACH DELETE n"
+    session.run(q)
+    q = "DROP CONSTRAINT ON (ateneo:ateneo) ASSERT ateneo._id IS UNIQUE"
+    session.run(q)
+    q = "CREATE CONSTRAINT ON (ateneo:ateneo) ASSERT ateneo._id IS UNIQUE"
+    session.run(q)
     q = """LOAD CSV FROM 'file:///Università.csv' AS line FIELDTERMINATOR ','
             CREATE (:ateneo {_id: toInteger(line[0]), cod: toInteger(line[1]), nomeesteso : line[2], nomeoperativo : line[3], status : line[4], tipologia : line[5], statale_nonstatale : line[6], indirizzo : line[7], comune : line[8], provincia : line[9], regione : line[10], zonageografica : line[11], url : line[12], dimensione : line[13]})"""
-    
-    r = execute_neo4j_query(q,session)
-    #for result in r:
-    #    print(result[0].labels)
+    session.run(q)
+
+    q = """LOAD CSV FROM 'file:///Laureati.csv' AS line FIELDTERMINATOR ';'
+            CREATE (:laureato {anno: toInteger(line[0]), codateneo: toInteger(line[1]), nomeateneo : line[2], sesso : line[3], numlaureati : toInteger(line[4])})"""
+    session.run(q)
+    q = """MATCH
+        (l:laureato),
+        (a:ateneo)
+        WHERE a.cod = l.codateneo  
+        CREATE (l)-[r:RELTYPE {name: 'laureati presso ' + a.nomeoperativo}]->(a)
+        RETURN type(r)"""
+    r = session.run(q)
+
+
+    session.close()
+    driver.close()
 
 if __name__ == "__main__":
     initPostgres()
-    #initNeo4j()
+    initNeo4j()
